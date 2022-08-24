63c714b480ffb66b8a8327c9943851790a268687
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-24 00:28:17 -0600

    got rid of a lot of comments and test codes for readability - try to solve first problem of mutating new array first...

--- without.js
+++ without.js
@@ -28,8 +28,6 @@ const assertArraysEqual = function (arrayA, arrayB) {
 // parameters should be: a source array && itemsToRemove array
 // return a new array with only those elements from source that are not present in the itemsToRemove array
 
-
-// Create a function that takes in two arrays, one original set, one a set of items to remove
 const without = (source, itemsToRemove) => {
   let newArray = source;
   for (const item of itemsToRemove) {
@@ -42,27 +40,9 @@ const without = (source, itemsToRemove) => {
       }
     }
   }
-  console.log(source);
-  console.log(newArray);
   return newArray;
-}
-
-// Edge Case; if an element in source === an element in itemsToRemove, we remove that item
-// we can instead push the elements that DO NOT === an element in itemsToRemove to a new array
-
-// make a new empty array to store our subset array into
-
-
+};
+// BUG - mutates original array...
 
 // TEST CODES
 console.log(without([1, 2, 3, 4], [3, 4])); // => [1, 2];
-// let arr1 = [1, 2, 3, 4];
-// console.log(without(arr1, [2])); // => [1, 3, 4]; also arr1 shouldn't change originally
-
-// console.log(without([1, 2, 3], [1])) // => [2, 3]
-// console.log(without(["hello", "hi", 3, "hola"], ["boy", "girl", 3])) // => ["hello", "hi", "hola"] 
-// console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-
-// // Use assertArraysEqual to write test cases for various scenarios.
-// assertArraysEqual([1, 2, 3], without(["1", "2", 1, "3", 2, 3], ["1", "2", "3"])); // => pass
-// assertArraysEqual([1, 2, 3], without(["1", "2", 1, "1", "2", 3, "3", 2], ["1", "2", "3"])); // [1, 2, 3] === [1, 3, 2] ? false => fail
