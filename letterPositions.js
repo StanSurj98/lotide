2397d24ea6b452e4c26857cfe3da299787e82930
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-24 17:51:27 -0600

    initial commit of letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,10 @@
+// --- Implement letterPositions --- 
+// will take in a string
+// return all the indices in the string where EACH character is found
+// for each letter, we will be returning multiple numbers, the places where the letter shows up
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  return results;
+};
