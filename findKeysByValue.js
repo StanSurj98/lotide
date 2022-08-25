784158415c12f793e4f402f4fe9f7ede7b2854e3
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-24 18:35:11 -0600

    solved core problem, passed tests

--- findKeysByValue.js
+++ findKeysByValue.js
@@ -12,9 +12,31 @@ const assertEqual = function(actual, expected) {
 // scans (object), and return the first KEY; which contains (value)
 // if no key found, return undefined
 
+const findKeyByValue = function(object, value) {
+  // we need to loop inside the keys of an object
+  for (const property in object) {
+    // console.log(`${property}: ${object[property]}`); // list out the keys and values in object param
+    if (object[property] === value) {
+      console.log(`Value: ${value} is inside property: ${property}`) // list out the property if matches value
+      // console.log(property);
+      return property;
+    }
+  }
+  // and also see the value of those keys
+  //  if those values === value (param), return object key as primitive string
+  // edge case: if no match, return undefined
+}
 
-
-
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
 
 
 // TEST CODES => Use assertEqual() but remember, it only compares primitives!
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // => pass: "drama" is primitive "string"
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy"); // => pass: "drama" is primitive "string"
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Rock n' Roll"), "music"); // => fail: undefined !== "music"
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // => pass: undefined is primitive
