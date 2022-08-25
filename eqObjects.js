c0761aa0bb9d14663a0f6a6c546a95f051955473
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-24 19:10:11 -0600

    Solved for primitives, but now onto detecting equal arrays

--- eqObjects.js
+++ eqObjects.js
@@ -16,15 +16,17 @@ const assertEqual = function(actual, expected) {
 const eqObjects = function(object1, object2) {
   // Four, try to implement logic on primitives first
     // 2 objects are equal when: 
-  console.log(`Obj1 keys: ${Object.keys(object1)}, Obj2 keys: ${Object.keys(object2)}`);
-  // - the NUMBER of keys are the same
+  console.log(`Obj1 keys: ${Object.keys(object1)} | Obj2 keys: ${Object.keys(object2)}`);
+  
+  // the NUMBER of keys are the same
   if (Object.keys(object1).length === Object.keys(object2).length) {
-    // - the VALUE of each key is the same
-    // meaning i have to loop through each value of each property in each object
+    // the VALUE of each key is the same
+    // loop through each value of each property in ONE object
     for (const property of Object.keys(object1)) {
-      // console.log(`${property} in Obj1 contains: ${object1[property]}`); // console what values are in the property of object1 during each iteration 
+      // if the value is the same, return true, the objects are truly equal
       if (object1[property] === object2[property]) {
-        console.log(object1[property] + " is equal to " + object2[property])
+        console.log(`Obj1 => Property: ${property}, Value: ${object1[property]}`)
+        console.log(`Obj2 => Property: ${property}, Value: ${object2[property]}`)
         return true;
       } else {
         return false;
@@ -36,14 +38,24 @@ const eqObjects = function(object1, object2) {
 };
 
 
-// Third, use assertEqual() to test simple things at first
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
+
+
+// Third, use assertEqual() to test simple things at first
+// assertEqual(ab, ba); // => fail; ab && ba are not primitives
+// assertEqual(ab.a, ba.a); // => pass; "1" === "1", primitives: string
+
+console.log(eqObjects(ab, ba)); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
+console.log(eqObjects(ab, abc)); // => false
 
 
-assertEqual(ab, ba); // => fail; ab && ba are not primitives
-assertEqual(ab.a, ba.a); // => pass; "1" === "1", primitives: string
+// Fifth, good job on primitives, but what if the values have arrays?
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
