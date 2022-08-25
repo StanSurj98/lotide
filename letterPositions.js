7c157b85a228b771a8854cd834e92896dded60c7
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-24 18:14:51 -0600

    passed more tests

--- letterPositions.js
+++ letterPositions.js
@@ -48,3 +48,6 @@ const letterPositions = function(sentence) {
 
 letterPositions("hello there"); 
 assertArraysEqual(letterPositions("hello").e, [1]); // => pass
+assertArraysEqual(letterPositions("lighthouse in the house").i, [1, 11]); // => pass
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]); // => pass
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 2, 20, 8]); // => fail
