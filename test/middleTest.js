fa96b004da8e62e9f79b771fd8b393957eaa23ab
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-29 17:13:40 -0600

    implemented mocha and chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,21 +1,33 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-
-
-
+// Update: implemented chai & mocha tests
 // TEST CODES
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])) // => [4, 5]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+describe("#middle", () => {
+  it("should return [] for arrays with less than 3 items", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-const arr1 = ['hello', 'hi', 'goodbye']
-const arr2 = ['boom', 'bang', 'foo', 'bar']
-console.log(middle(arr1)); // => ['hi']
-console.log(middle(arr2)); // => ['bang', 'foo']
+  it("should return ['hi'] for ['hello', 'hi', 'goodbye']", () => {
+    const init = ['hello', 'hi', 'goodbye'];
+    const expected = ['hi'];
+    assert.deepEqual(middle(init), expected);
+  });
+  
+  it("should return ['bang', 'foo'] for ['boom', 'bang', 'foo', 'bar']", () => {
+    const init = ['boom', 'bang', 'foo', 'bar'];
+    const expected = ['bang', 'foo'];
+    assert.deepEqual(middle(init), expected);
+  });
+  
+  it("should return [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    const init = [1, 2, 3, 4, 5, 6, 7, 8];
+    const expected = [4, 5];
+    assert.deepEqual(middle(init), expected);
+  });
+  
+  it("should return [1] for [0, 1, 2]", () => {
+    assert.deepEqual(middle([0, 1, 2]), [1]);
+  });
 
-// Test assertions
-// Use assertArraysEqual function to test our new func
-assertArraysEqual(middle([1, 2, 3, 4, 5]), middle([2, 3, 3, 4, 6])); // => pass; since middle is still [3]
-
-assertArraysEqual(middle(['foo', 'bar', 'hey', 'hello']), middle(['world', 'bar', 'hey', 'yo'])); // => pass; ['bar', 'hey']
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,30 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-// TEST CODES
-const words = ['yo', 'what', 'is', 'up'];
-const tailOfWords = tail(words);
+// New chai style test codes
+describe("#tail", () => {
+  it("should return ['what', 'is', 'up'] for ['yo', 'what', 'is', 'up']", () => {
+    const words = ['yo', 'what', 'is', 'up']; 
+    const expected =  ['what', 'is', 'up'];
+    assert.deepEqual(tail(words), expected);
+  });
 
+  it("should return [1, 2, 3, 4, 5] for [0, 1, 2, 3, 4, 5]", () => {
+    const initial = [0, 1, 2, 3, 4, 5];
+    const expected = [1, 2, 3, 4, 5];
+    assert.deepEqual(tail(initial), expected);
+  });
 
-assertEqual(tailOfWords.length, 3); // pass
-assertEqual(words.length, 3); // fail
-assertEqual(tail([0, 1, 2, 3, 4, 5]).length, 5); // pass
+  it("should return an empty array for []", () => {
+    const initial = [];
+    const expected = [];
+    assert.deepEqual(tail(initial), expected);
+  });
 
-console.log('-------------------');
+  it("should return an empty array for single item [1]", () => {
+    const initial = [1];
+    const expected = [];
+    assert.deepEqual(tail(initial), expected);
+  });
 
-// must check these test down here
-assertEqual(tail([]), 0); // fail
-console.log(tail([]));
-console.log(tail([1]));
+});
