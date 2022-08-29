5b399dcb458c6efa77e59301b196fb5ed24b8790
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-29 16:17:23 -0600

    implemented mocha and chai testing vs my own

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,35 @@
-const assertEqual = require("../assertEqual");
-const head = require("../head");
+// Update: commented out because we're now integrating mocha and chai tests
+
+// const assertEqual = require("../assertEqual");
+// const head = require("../head");
+
+
 
 // TEST CODES
-assertEqual(head([6, 3, 7, 1]), 6); // should pass
-assertEqual(head(['blah', 'heyo', 'hiya']), 'heyoo'); // should fail
-assertEqual(head([]), undefined); // should pass, I want an empty array [] to return undefined
-assertEqual(head([1]), 0); // should fail, single item should be that item
-assertEqual(head([1]), 1); // pass
+// Update: converted my test codes to chai style and ran with mocha
+
+// Update: We're going to be implementing mocha and chai's tests
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5')
+  });
+
+  it("should return 6 for [6, 3, 7, 1]", () => {
+    assert.strictEqual(head([6, 3, 7, 1]), 6)
+  });
+
+  it("should return 'blah' for ['blah', 'heyo', 'hiya']", () => {
+    assert.strictEqual(head(['blah', 'heyo', 'hiya']), 'blah')
+  });
+
+  it("should return undefined for []", () => {
+    assert.strictEqual(head([]), undefined)
+  });
+});
